7ddc5a612350c621b9f19e3c2d9b31480f835738
CaetanoVidal <calmgv@gmail.com>
2022-01-12 20:46:24 -0300

    trying to make this work

--- test/await_and_async.py
+++ test/await_and_async.py
@@ -6,6 +6,16 @@ async def do_stuff(ip, port):
     print(f'About to open a connection to {ip}'.format(ip=ip))
     reader, writer = await asyncio.open_connection(ip, port)
 
-    print(f'Connection open to {ip}'.format(ip=ip))
+    print(f'Connection open to {ip}')
     await asyncio.sleep(randint(0, 5))
-    
+
+    writer.close()
+    print(f'Closed connection to {ip}')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    work = [
+        asyncio.ensure_future(do_stuff('8.8.8.8', '53')),
+        asyncio.ensure_future(do_stuff('8.8.4.4', '53'))
+    ]
